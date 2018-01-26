73e753857d8195b530ef05142803c468cf709e4a
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-01-26 17:00:45 -0500

    fix issue for changepw and sign out
    did not need user.id for changepw and sign out. fixed

--- assets/scripts/api/authApi.js
+++ assets/scripts/api/authApi.js
@@ -20,7 +20,7 @@ const signIn = function (data) {
 
 const changePass = function (data) {
   return $.ajax({
-    url: config.apiOrigin + '/change-password/' + store.user.id,
+    url: config.apiOrigin + '/change-password',
     method: 'PATCH',
     headers: { Authorization: 'Token token=' + store.user.token },
     data
@@ -29,7 +29,7 @@ const changePass = function (data) {
 
 const signOut = function () {
   return $.ajax({
-    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    url: config.apiOrigin + '/sign-out',
     method: 'DELETE',
     headers: { Authorization: 'Token token=' + store.user.token }
   })
